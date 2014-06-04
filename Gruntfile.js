c465eb91bb5d2fdb657bb8254a5d24610a6aeb85
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-03 22:44:54 -0700

    watch and run tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-dox');
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-
+    grunt.initConfig({
+        watch: {
+            tests: {
+                files: ['tests/spec/**/*.js'],
+                tasks: ['mocha:test']
+            }
+        },
+        mocha: {
+          test: {
+            src: ['tests/index.html'],
+            options: {
+                run: true
+            }
+          },
+        }
+    });
 };
