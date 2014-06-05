0a74360b23580bd67f28d60f7c3f16141d6ebca7
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-05 10:42:59 -0700

    was testing the wrong thing

--- tests/spec/test-query-util.js
+++ tests/spec/test-query-util.js
@@ -17,10 +17,10 @@
 
         describe('#toQueryFormat()', function () {
             it('should convert single-key objects to string', function () {
-                qutils.toMatrixFormat({a: 2}).should.equal('a=2');
+                qutils.toQueryFormat({a: 2}).should.equal('a=2');
             });
             it('should convert multi-key objects to semicolon-seperated values', function () {
-                qutils.toMatrixFormat({a: 2, b: 3}).should.equal('a=2&b=3');
+                qutils.toQueryFormat({a: 2, b: 3}).should.equal('a=2&b=3');
             });
         });
     });
