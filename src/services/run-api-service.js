13c04be16c13eda267c4fcb114170182c5552190
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-05 07:31:06 -0700

    document options

--- src/services/run-api-service.js
+++ src/services/run-api-service.js
@@ -72,7 +72,8 @@ module.exports = function (config) {
          * Parameters to filter the list of runs by
          * @param {Object} qs Query
          * @param {Object} limit | page | sort @see <TBD>
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.query({
                     'saved': 'true',
@@ -98,6 +99,7 @@ module.exports = function (config) {
          * Similar to query, except merges parameters instead of over-writing them
          * @param {Object} filter
          * @param {Object} limit | page | sort @see <TBD>
+         * @param {object} options Overrides for configuration options
 
          * @example
          *     rs.query({
@@ -118,7 +120,8 @@ module.exports = function (config) {
          * Get data for a specific run
          * @param  {String} runID
          * @param  {Object} filters
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.get('<runid>', {include: '.score', set: 'xyz'});
          */
@@ -132,7 +135,8 @@ module.exports = function (config) {
          * @see  variables service to see what you can do with it
          * @param  {String} variableSet (Optional)
          * @param  {Object} filters (Optional)
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.variables(["Price", "Sales"])
          *     rs.variables()
@@ -145,7 +149,8 @@ module.exports = function (config) {
         /**
          * Save attributes on the run
          * @param  {Object} attributes Run attributes to save
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.save({completed: true});
          *     rs.save({saved:true, variables: {a:23,b:23}});
@@ -160,7 +165,8 @@ module.exports = function (config) {
          * Call an operation on the model
          * @param  {String} operation Name of operation
          * @param  {*} params   (Optional) Any parameters the operation takes
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.do('solve');
          *     rs.do('add', [1,2]);
@@ -174,7 +180,8 @@ module.exports = function (config) {
          * Call a bunch of operations in serial
          * @param  {Array<string>} operations List of operations
          * @param  {params} params     Parameters for each operation
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.serial(['initialize', 'solve', 'reset']);
          *     rs.serial([{name: add, params: [1,2]]}, {name:'subtract', params:[2,3]});
@@ -186,7 +193,8 @@ module.exports = function (config) {
         /**
          * Executes operations in parallel
          * @param  {Array|Object} operations List of operations and arguments (if object)
-         *
+         * @param {object} options Overrides for configuration options
+          *
          * @example
          *     rs.parallel({add: [1,2], subtract: [2,4]});
          *     rs.parallel([{name: add, params: [1,2]]}, {name:'subtract', params:[2,3]});
