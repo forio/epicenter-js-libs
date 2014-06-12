b306d43e653008674a95389d37328cdf34501a0b
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-12 09:22:52 +0530

    test url override

--- tests/spec/test-ajax-http-transport.js
+++ tests/spec/test-ajax-http-transport.js
@@ -7,10 +7,10 @@
         var server;
         before(function () {
             server = sinon.fakeServer.create();
-            server.respondWith(/api\.success\.com/, function (xhr, id){
+            server.respondWith(/api\.success/, function (xhr, id){
                 xhr.respond(200, { 'Content-Type': 'application/json'}, JSON.stringify({url: xhr.url}));
             });
-            server.respondWith(/api\.fail\.com/, function (xhr, id){
+            server.respondWith(/api\.fail/, function (xhr, id){
                 xhr.respond(500, { 'Content-Type': 'application/json'}, JSON.stringify({url: xhr.url}));
             });
             server.autoRespond = true;
@@ -85,6 +85,17 @@
                 req.url.should.equal('http://api.success.com');
                 req.requestBody.should.equal(JSON.stringify(params));
             });
+
+            it('should override the url', function () {
+                var params = {a:2,b:3};
+                var ajax = new Transport({url: 'http://api.success.com'});
+                ajax.post(params, {url: 'http://api.success.org'});
+
+                var req = server.requests.pop();
+                req.url.should.equal('http://api.success.org');
+                req.requestBody.should.equal(JSON.stringify(params));
+            });
+
             it('should combine query parameters', function () {
                 var params = {b:3};
                 var ajax = new Transport({url: 'http://api.success.com?a=2'});
