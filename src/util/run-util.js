aa2080f20c57365ff57db9ead6f66b5aace98221
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-12 09:15:43 +0530

    Operation normalize utility

---
+++ src/util/run-util.js
@@ -0,0 +1,69 @@
+/**
+ * Utilities for working with the run service
+*/
+(function(){
+var root = this;
+var F = root.F;
+
+var run= function () {
+
+    return {
+        /**
+         * returns operations of the form [[op1,op2], [arg1, arg2]]
+         * @param  {Object|Array|String} operations operations to perform
+         * @param  {Array} arugments for operation
+         * @return {String}    Matrix-format query parameters
+         */
+        normalizeOperations: function(operations, args) {
+            if (!args) args = [];
+
+            var opsList = [];
+            var argsList = [];
+
+            if ($.isPlainObject(operations)) {
+                //{add: [1,2], subtract: [2,4]}
+                $.each(operations, function(opn, arg) {
+                    opsList.push(opn);
+                    argsList.push(arg);
+                });
+            }
+            else if($.isArray(operations)) {
+                // ['solve', 'reset']
+                $.each(operations, function(index, opn) {
+                    if ($.isPlainObject(opn)) {
+                        //{name: add, params: [1,2]]}, {name: 'subtract', params:[2,3]}
+                        if (opn.name) {
+                            opsList.push(opn.name);
+                            argsList.push(opn.params);
+                        }
+                        else {
+                            //{add: [1,2], subtract: [2,4]}
+                            $.each(opn, function(op, arg) {
+                                opsList.push(op);
+                                argsList.push(arg);
+                            });
+                        }
+                    }
+                    else {
+                        //'solve'
+                        opsList.push(opn);
+                        argsList.push(args[index]);
+                    }
+                });
+            }
+            return [opsList, argsList];
+        }
+    };
+}();
+
+
+if (typeof exports !== 'undefined') {
+    module.exports = run;
+}
+else {
+    if (!root.F) { root.F = {};}
+    if (!root.F.util) { root.F.util = {};}
+    root.F.util.run = run;
+}
+
+}).call(this);
---
+++ tests/spec/test-run-util.js
@@ -0,0 +1,40 @@
+(function() {
+
+    var rutil = F.util.run;
+
+    describe('Run Utilities', function () {
+       describe('#normalizeOperations()', function () {
+
+            it('takes in named object pairs', function () {
+                var params =  [{name: 'add', params: [1,2]}, {name: 'subtract', params: [2,3]}];
+                var result = rutil.normalizeOperations(params);
+                result.should.deep.equal([['add', 'subtract'], [[1,2], [2,3]]]);
+            });
+
+            it('takes in anon objects', function () {
+                var params =  {add: [1,2], subtract: [2,3]};
+                var result = rutil.normalizeOperations(params);
+                result.should.deep.equal([['add', 'subtract'], [[1,2], [2,3]]]);
+            });
+
+            it('takes in array pairs', function () {
+                var params =  ['add', 'subtract'];
+                var args = [[1,2], [2,3]];
+                var result = rutil.normalizeOperations(params, args);
+                result.should.deep.equal([['add', 'subtract'], [[1,2], [2,3]]]);
+            });
+
+            it('takes in single arrays', function () {
+                var params =  ['init', 'reset'];
+                var result = rutil.normalizeOperations(params);
+                result.should.deep.equal([['init', 'reset'],[undefined, undefined]]);
+            });
+
+            it('takes in single arrays and objects', function() {
+                var params =  ['init', 'reset', {add: [1,2]}];
+                var result = rutil.normalizeOperations(params);
+                result.should.deep.equal([['init', 'reset', 'add'],[undefined, undefined, [1,2]]]);
+            });
+        });
+    });
+}());
