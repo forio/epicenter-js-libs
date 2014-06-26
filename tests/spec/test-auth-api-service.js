0514d5085d324f6df72b69cf8067365ee6e337fe
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-26 10:40:32 -0700

    updated auth tests

--- tests/spec/test-auth-api-service.js
+++ tests/spec/test-auth-api-service.js
@@ -17,21 +17,36 @@
         });
 
         describe('#login', function () {
+            it('should require username and password', function () {
+                var as = new AuthService();
+                (function(){ as.login();}).should.throw(Error);
+            });
+
             it('should do a GET', function () {
                 var as = new AuthService();
-                as.login('user', 'pass');
+                as.login({userName: 'john', password: 'y'});
 
                 var req = server.requests.pop();
                 req.method.toUpperCase().should.equal('GET');
             });
             it('should send requests to body', function () {
                 var as = new AuthService();
-                as.login('user', 'pass');
+                as.login({userName: 'john', password: 'y'});
 
                 var req = server.requests.pop();
                 req.method.toUpperCase().should.equal('GET');
                 req.url.should.equal('https://api.forio.com/authentication/');
-                req.requestBody.should.equal(JSON.stringify({username: 'user', password: 'pass'}));
+                req.requestBody.should.equal(JSON.stringify({userName: 'john', password: 'pass'}));
+            });
+
+            it('should pick up creds from service options', function () {
+                var as = new AuthService({userName: 'john', password: 'y'});
+                as.login();
+
+                var req = server.requests.pop();
+                req.method.toUpperCase().should.equal('GET');
+                req.url.should.equal('https://api.forio.com/authentication/');
+                req.requestBody.should.equal(JSON.stringify({userName: 'john', password: 'pass'}));
             });
         });
         describe('#logout', function () {
