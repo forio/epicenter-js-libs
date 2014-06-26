c33e3ffb43cbdf009ee5f4c3fc6945368aaf15e1
Naren Ranjit <narendran.ranjit@gmail.com>
2014-06-25 17:26:46 -0700

    grunt production task

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,5 +90,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('documentation', ['markdox']);
+    grunt.registerTask('production', ['uglify:production']);
 
 };
