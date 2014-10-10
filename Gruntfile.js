020bba556a7054f2bf13275e9fe6a68e1dc342a1
Naren Ranjit <narendran.ranjit@gmail.com>
2014-10-09 22:49:01 -0700

    basic browserify framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
     'use strict';
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
     grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-markdox');
@@ -8,9 +12,65 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jscs-checker');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+
+    grunt.loadNpmTasks('grunt-browserify2');
+    var UglifyJS = require('uglify-js');
+
+    grunt.config.set('browserify2', {
+        options: {
+            expose: {
+
+            },
+            entry: './src/app.js'
+        },
+        // tests: {
+        //     options: {
+
+        //         entry: './tests/test-list.js',
+        //         compile: './tests/tests-browserify-bundle.js',
+        //         debug: true
+        //     }
+        // },
+        // edge: {
+        //     options: {
+        //         debug: true,
+        //         compile: './dist/flow-edge.js'
+        //     }
+        // },
+        // mapped: {
+        //     options: {
+        //         debug: true,
+        //         compile: './dist/flow.js'
+        //     },
+        //     afterHook: function(src) {
+        //         var banner = grunt.file.read('./banner.js');
+        //         banner = grunt.template.process(banner, {data: grunt.file.readJSON('package.json')});
+        //         return banner + src;
+        //     }
+        // },
+        min: {
+            options: {
+                debug: false,
+                compile: './dist/epicenter.min.js'
+            },
+            afterHook: function(src) {
+                var result = UglifyJS.minify(src, {
+                    fromString: true,
+                    warnings: true,
+                    mangle: true,
+                    compress:{
+                        pure_funcs: [ 'console.log' ]
+                    }
+                });
+                var code = result.code;
+                // var banner = grunt.file.read('./banner.js');
+                // banner = grunt.template.process(banner, {data: grunt.file.readJSON('package.json')});
+                return code;
+            }
+        }
     });
+
+
 
     grunt.loadNpmTasks('grunt-conventional-changelog');
     grunt.config.set('changelog',{
---
+++ src/app.js
@@ -0,0 +1,10 @@
+var F = {
+    util: {},
+    factory: {},
+    transport: {},
+    store: {},
+    service: {},
+    manager: {}
+};
+
+F.util.query = require('./util/query-util');
