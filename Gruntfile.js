79a552b22f3fe064675d4896f48b3582b9d37817
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-10-10 08:57:36 -0700

    Changed grunt to build unmagnified version when watching

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
     grunt.config.set('watch', {
         source: {
             files: ['src/**/*.js'],
-            tasks: ['mocha:test', 'uglify:production']
+            tasks: ['mocha:test', 'uglify']
         },
         tests: {
             files: ['tests/spec/**/*.js'],
@@ -201,5 +201,5 @@ module.exports = function(grunt) {
         });
     });
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['mocha:test', 'uglify', 'watch']);
 };
