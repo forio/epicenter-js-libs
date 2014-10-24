e50253e1c45bbff6cdb43c2b28b7f9c463821bfa
Molly Jones <mjones@forio.com>
2014-10-24 14:27:33 -0700

    move main comment block to top of file; fix some links

--- src/managers/run-manager.js
+++ src/managers/run-manager.js
@@ -1,3 +1,48 @@
+/**
+* ## Run Manager 
+* 
+* The Run Manager gives you control over run creation depending on run states. Specifically, you can select run creation strategies (rules) for which runs end users of your project work with when they log in to your project.
+*
+* Underlying Epicenter APIs -- including the [Model Run API](../../../model_apis/run/), the [Run API](../../../aggregate_run_api), and Epicenter.js's own [F.service.Run.create()](../run-api-service/) -- all allow you to create new runs. Of course, you can create new runs with the Run Manager. However, for some projects it makes more sense to pick up where the user left off, using an existing run. And in some projects, whether to create a new one or use an existing one is conditional, for example based on characteristics of the existing run or your own knowledge about the model. The Run Manager provides this level of control.
+*
+* ### Using the Run Manager to create and access runs
+*
+* To use the Run Manager, instantiate it by passing in: 
+*
+*   * `run`: (required) Run object. Must contain:
+*       * `account`: Epicenter account id (**Team ID** for team projects, **User ID** for personal projects).
+*       * `project`: Epicenter project id.
+*       * `model`: The name of your primary model file. (See more on [Writing your Model](../../writing_your_model/).)
+*       * `scope`: (optional) Scope object for the run, for example `scope.group` with value of the name of the group.
+*       * `files`: (optional) If and only if you are using a Vensim model and you have additional data to pass in to your model, you can pass a `files` object with the names of the files, for example: `"files": {"data": "myExtraData.xls"}`. (Note that you'll also need to add this same files object to your Vensim [configuration file](../../../model_code/vensim/).) See the [underlying Model Run API](../../../model_apis/run/#post-creating-a-new-run-for-this-project) for additional information. 
+*
+*   * `strategy`: (optional) Run creation strategy for when to create a new run and when to reuse an end user's existing run. See [Run Manager Strategies](../../strategy/) for details. Defaults to `new-if-simulated`.
+*
+*   * `sessionKey`: (optional) Name of browser cookie in which to store run information, including run id. Many conditional strategies, including the provided strategies, rely on this browser cookie to store the run id and help make the decision of whether to create a new run or use an existing one. The name of this cookie defaults to `epicenter-scenario` and can be set with the `sessionKey` parameter.
+*
+*
+* After instantiating a Run Manager, make a call to `getRun()` whenever you need to access a run for this end user. The `RunManager.run` contains the instantiated [Run Service](../run-api-service/).
+*
+* **Example**
+*
+*       var rm = new F.manager.RunManager({
+*           run: {
+*               account: 'acme-simulations',
+*               project: 'supply-chain-game',
+*               model: 'supply-chain-model.jl',
+*               server: { host: 'api.forio.com' }
+*           },
+*           strategy: 'always-new',
+*           sessionKey: 'epicenter-session'
+*       });
+*       rm.getRun()
+*           .then(function(run) { 
+*               var thisRunId = run.id; // the return value of getRun() is the run object itself
+*               rm.run.do('runModel');  // the RunManager.run contains the instantiated Run Service, so any Run Service method is valid here
+*       })
+*
+*/
+
 'use strict';
 var strategiesMap = require('./run-strategies/strategies-map');
 var specialOperations = require('./special-operations');
@@ -24,66 +69,6 @@ function patchRunService(service, manager) {
     return service;
 }
 
-/**
-* ## Run Manager 
-* 
-* The Run Manager gives you control over run creation depending on run states. Specifically, you can select run creation strategies (rules) for which runs end users of your project work with when they log in to your project.
-*
-* Underlying Epicenter APIs -- including the [Model Run API](../../../model_code/run/), the [Run API](../../../aggregate_run_api), and Epicenter.js's own [F.service.Run.create()](../../run-api-service/) -- all allow you to create new runs. Of course, you can create new runs with the Run Manager. However, for some projects it makes more sense to pick up where the user left off, using an existing run. And in some projects, whether to create a new one or use an existing one is conditional, for example based on characteristics of the existing run or your own knowledge about the model. The Run Manager provides this level of control.
-*
-* ### Using the Run Manager to create and access runs
-*
-* To use the Run Manager, instantiate it by passing in: 
-*
-*   * `run`: (required) Run object. Must contain:
-*       * `account`: Epicenter account id (**Team ID** for team projects, **User ID** for personal projects).
-*       * `project`: Epicenter project id.
-*       * `model`: The name of your primary model file. Simulation model to create the run against.
-*       * `scope`: (optional) Scope object for the run, for example `scope.group` with value of the name of the group.
-*       * `files`: (optional) If and only if you are using a Vensim model and you have additional data to pass in to your model, you can pass a `files` object with the names of the files, for example: `"files": {"data": "myExtraData.xls"}`. (Note that you'll also need to add this same files object to your Vensim [configuration file](../../../model_code/vensim/).) See the [underlying Model Run API](../../../model_apis/run/##post-creating-a-new-run-for-this-project) for additional information. 
-*
-*   * `strategy`: (optional) Run creation strategy for when to create a new run and when to reuse an end user's existing run. See [Run Manager Strategies](../../strategy/) for details. Defaults to `new-if-simulated`.
-*
-*   * `sessionKey`: (optional) Name of browser cookie in which to store run information, including run id. Many conditional strategies -- including the provided strategies -- rely on this browser cookie to store the run id and help make the decision of whether to create a new run or use an existing one. The name of this cookie defaults to `epicenter-scenario` and can be set with the `sessionKey` parameter.
-*
-*
-* After instantiating a Run Manager, make a call to `getRun()` whenever you need to access a run for this end user. The `RunManager.run` contains the instantiated [Run Service](../run-api-service/).
-*
-* **Example**
-*
-*       var rm = new F.manager.RunManager({
-*           run: {
-*               account: 'acme-simulations',
-*               project: 'supply-chain-game',
-*               model: 'supply-chain-model.jl',
-*               server: { host: 'api.forio.com' }
-*           },
-*           strategy: 'always-new',
-*           sessionKey: 'epicenter-session'
-*       });
-*       rm.getRun()
-*           .then(function(run) { 
-*               var thisRunId = run.id; // the return value of getRun() is the run object itself
-*               rm.run.do('runModel');  // the RunManager.run contains the instantiated Run Service, so any Run Service method is valid here
-*       })
-*
-*         
-* @param {object} `options` The options object to configure the manager and run.
-*
-*   - `run`: (required) Run object, including:
-*       `account`: Epicenter account id (Team ID for team projects, User ID for personal projects).
-*       `project`: Epicenter project id.
-*       `model`: The name of your primary model file. Simulation model to create the run against.
-*       `scope`: (optional) Scope object for the run, for example `scope.group` with value of the name of the group.
-*       `files`: (optional) If and only if you are using a Vensim model and you have additional data to pass in to your model, you can pass a `files` object with the names of the files, for example: `"files": {"data": "myExtraData.xls"}`. (Note that you'll also need to add this same files object to your Vensim [configuration file](../../../model_code/vensim/).) See the [underlying Model Run API](../../../model_apis/run/##post-creating-a-new-run-for-this-project) for additional information. 
-*        Other: other options to pass to the run service instance (ie. options.run = runService ) ...
-*
-*   - `strategy`: (optional) Run creation strategy. Default: `new-if-persisted`.
-*
-*   - `sessionKey`: (optional) Name of browser cookie in which to store run information, including run id. This cookie is checked by many of the provided strategies.
-*
-*
-**/
 
 var defaults = {
     /**
@@ -94,6 +79,7 @@ var defaults = {
 
     strategy: 'new-if-simulated'
 };
+
 
 function RunManager(options) {
     this.options = $.extend(true, {}, defaults, options);
@@ -122,7 +108,7 @@ RunManager.prototype = {
      * A good run is defined by the strategy. For example, if the strategy is `always-new`, the call
      * to `getRun()` always returns a newly created run; if the strategy is `new-if-persisted`, 
      * `getRun()` creates a new run if the previous run is in a persisted state, otherwise
-     * it returns the previous run.
+     * it returns the previous run. See [Run Manager Strategies](../../strategy/) for more on strategies.
      *
      *  **Example**
      *
