c778a99df779b5e952d00a6125a5edd410bea6ea
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-01-28 10:01:30 -0800

    Updated identity strategy to have the interface for strategies

--- src/managers/run-strategies/identity-strategy.js
+++ src/managers/run-strategies/identity-strategy.js
@@ -2,4 +2,13 @@
 var classFrom = require('../../util/inherit');
 var Base = {};
 
-module.exports = classFrom(Base, {});
+// Interface that all strategies need to implement
+module.exports = classFrom(Base, {
+    reset: function () {
+        // return a newly created run
+    },
+
+    getRun: function () {
+        // return a usable run
+    }
+});
