bd40bd353770988c8f6d156e1e80c5f449d73780
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-01-29 15:29:06 -0800

    Completed functionality for multiplayer strategy

--- src/managers/run-strategies/multiplayer-strategy.js
+++ src/managers/run-strategies/multiplayer-strategy.js
@@ -14,54 +14,58 @@ var defaults = {
     }
 };
 
-var store = new StorageFactory();
-
 var Strategy = classFrom(IdentityStrategy, {
+
     constructor: function (runService, options) {
         this.runService = runService;
         this.options = $.extend(true, {}, defaults, options);
-        store = new StorageFactory(this.options.store);
-
+        this._store = new StorageFactory(this.options.store);
     },
 
     reset: function () {
-
+        throw new Error('Not supported. Reset should not be called from the end-user side');
     },
 
     getRun: function () {
-        var session = JSON.parse(store.get(keyNames.EPI_SESSION_KEY) || '{}');
+        var session = JSON.parse(this._store.get(keyNames.EPI_SESSION_KEY) || '{}');
+        var curUserId = session.userId;
         var opt = $.extend({
             account: session.account,
             project: session.project,
             group: session.groupName
         }, _pick(this.options, ['account', 'project', 'group']));
-        var curUserId = session.userId;
+
+        var dtd = $.Deferred();
 
         if (!curUserId) {
-            throw new Error('We need an authenticated user to join a multiplayer game. (ERR: no userId in session)');
+            return dtd.reject({ statusCode: 400, error: 'We need an authenticated user to join a multiplayer game. (ERR: no userId in session)' }, session).promise();
         }
 
         var game = new GameApiAdapter(opt);
 
-        var restoreInitRun = function (games) {
+        var restoreInitRun = function (games, msg, xhr) {
             if (!games || !games.length) {
-                throw new Error('The user is not in any game.');
+                return dtd.reject({ statusCode: 404, error: 'The user is not in any game.' }, { options: opt, session: session, xhr: xhr });
             }
 
+            // assume the most recent game as the 'active' game
             games.sort(function (a, b) { return new Date(b.lastModified) - new Date(a.lastModified); });
 
-            return game.getCurrentRun({ filter: games[0].id });
+            return game.getCurrentRun({ filter: games[0].id })
+                .then(dtd.resolve)
+                .fail(dtd.reject);
         };
 
-        var userNotInGame = function (error) {
+        var serverError = function (error) {
             // is this possible?
-            throw new Error('The current user is not in any game', error);
+            dtd.reject(error, session, opt);
         };
 
-        return game
-            .getGamesForUser({ userId: curUserId })
+        game.getGamesForUser({ userId: curUserId })
             .then(restoreInitRun)
-            .fail(userNotInGame);
+            .fail(serverError);
+
+        return dtd.promise();
     }
 });
 
