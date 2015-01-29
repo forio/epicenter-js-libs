63387fc806e5c576d4b988cfc30acb8c2d8b7b80
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-01-28 16:43:08 -0800

    PR Feedback and fixed a few issues

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -7,10 +7,20 @@
         $('#login-message').text(msg).show();
     };
 
-    var selectGroup = function (userName, password, account, project, groups) {
+    var getAccountProjectFromUrl = function () {
+        var url = window.location.href;
+        var parts = url.match(/https?:\/\/(.*)\/app\/(.*)\/(.*)/gi) || [];
+        return {
+            account: parts[2],
+            project: parts[3]
+        };
+    };
+
+    var selectGroup = function (userName, password, account, project, groups, action) {
         var dlg = (dlg = $('.group-selection-dialog')).length ? dlg : $(groupSelectionTemplate).appendTo($('body'));
-        var select = $('#log-to-group', dlg);
-        select.find('[value]').remove();
+        dlg.attr('action', action);
+        var select = $('#login-group', dlg);
+        select.find('[value!=""]').remove();
         $.each(groups, function () {
             $('<option>')
                 .attr('value', this.groupId)
@@ -19,7 +29,14 @@
         });
 
         select.on('change', function (e) {
-            $('#forio-login-groupId').val($(e.target).val());
+            var val = $(e.target).val();
+            $('#groupId').val(val);
+
+            if (val) {
+                $('#btn-login', dlg).removeClass('disabled').removeAttr('disabled');
+            } else {
+                $('#btn-login', dlg).addClass('disabled').attr('disabled', 'disabled');
+            }
         });
 
         var closeDlg = function () {
@@ -31,18 +48,28 @@
         $('.group-selection-dialog').show();
     };
 
-    $('#login, form:has(#btn-login)').on('submit', function (e) {
+    $('body').on('submit', '#login, form:has(#btn-login)', function (e) {
         e.preventDefault();
+        e.stopPropagation();
         var form = $(e.target);
-        var action = form.attr('action') || 'index.html';
-        var userName = $('#forio-login-username').val();
-        var password = $('#forio-login-password').val();
-        var account = $('#forio-login-account').val();
-        var project = $('#forio-login-project').val();
-        var groupId = ($('#forio-login-groupId').length ? $('#forio-login-groupId') : $('<input type="hidden" id="forio-login-groupId">').appendTo(form)).val();
+        var fromUrl = getAccountProjectFromUrl();
+        var action = form.prop('action') || 'index.html';
+        var userName = $('#username').val();
+        var password = $('#password').val();
+        var account = $('#account').val() || fromUrl.account;
+        var project = $('#project').val() || fromUrl.project;
+        var groupId = ($('#groupId').length ? $('#groupId') : $('<input type="hidden" id="groupId">').appendTo(form)).val();
 
         $('button', form).prop('disabled', 'disabled').addClass('disabled');
         $('#login-message').text('').hide();
+
+        if (!account) {
+            console.log('No account was specified and it cannot be extracted from the URL. You may not be able to login.');
+        }
+
+        if (!project) {
+            console.log('No project was specified and it cannot be extracted from the URL.');
+        }
 
         var auth = new F.manager.AuthManager();
         auth.login({
@@ -56,7 +83,7 @@
             if (error.status === 401) {
                 showError('Invalid user name or password.', error.status);
             } else if (error.status === 403) {
-                selectGroup(userName, password, account, project, error.userGroups);
+                selectGroup(userName, password, account, project, error.userGroups, action);
             } else {
                 showError('Unknown error occured. Please try again. (' + error.status + ')', error.status);
             }
@@ -77,13 +104,13 @@
             <div class="panel panel-default form-inline"> \
                 <div class="panel-heading"><span class="panel-title">Please select a group</span><span class="close pull-right">x</span></div> \
                 <div class="panel-body"> \
-                    <label for="log-to-group">Group:</label> \
-                    <select name="log-to-group" id="log-to-group" class="form-control"> \
-                        <option default>Please select a group</option> \
+                    <label for="login-group">Group:</label> \
+                    <select name="login-group" id="login-group" class="form-control"> \
+                        <option default value="">Please select a group</option> \
                     </select> \
                 </div> \
                 <div class="panel-footer clearfix"> \
-                    <button id="btn-login" class="pull-right btn btn-primary">Select Group</button> \
+                    <button id="btn-login" disabled class="pull-right btn btn-primary disabled">Select Group</button> \
                 </div> \
             </div> \
         </div> \
