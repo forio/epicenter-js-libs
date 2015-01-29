92bca75b6663477b229003bc861c6de07d8bb33f
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-01-29 11:58:30 -0800

    Implemented basics of multiplayer strategy using data stored in the session cookie

--- src/managers/run-strategies/multiplayer-strategy.js
+++ src/managers/run-strategies/multiplayer-strategy.js
@@ -3,15 +3,25 @@
 var classFrom = require('../../util/inherit');
 
 var IdentityStrategy = require('./identity-strategy');
-// var StorageFactory = require('../store/store-factory');
+var StorageFactory = require('../../store/store-factory');
 var GameApiAdapter = require('../../service/game-api-adapter');
+var keyNames = require('../key-names');
+var _pick = require('../../util/object-util')._pick;
 
-var defaults = {};
+var defaults = {
+    store: {
+        synchronous: true
+    }
+};
+
+var store = new StorageFactory();
 
 var Strategy = classFrom(IdentityStrategy, {
     constructor: function (runService, options) {
         this.runService = runService;
         this.options = $.extend(true, {}, defaults, options);
+        store = new StorageFactory(this.options.store);
+
     },
 
     reset: function () {
@@ -19,16 +29,32 @@ var Strategy = classFrom(IdentityStrategy, {
     },
 
     getRun: function () {
-        var curUserId = this.options.userId;
-        // var groupName = this.options.group;
-        var game = new GameApiAdapter(this.options);
+        var session = JSON.parse(store.get(keyNames.EPI_SESSION_KEY) || '{}');
+        var opt = $.extend({
+            account: session.account,
+            project: session.project,
+            group: session.groupName
+        }, _pick(this.options, ['account', 'project', 'group']));
+        var curUserId = session.userId;
+
+        if (!curUserId) {
+            throw new Error('We need an authenticated user to join a multiplayer game. (ERR: no userId in session)');
+        }
+
+        var game = new GameApiAdapter(opt);
 
         var restoreInitRun = function (games) {
+            if (!games || !games.length) {
+                throw new Error('The user is not in any game.');
+            }
+
             games.sort(function (a, b) { return new Date(b.lastModified) - new Date(a.lastModified); });
-            return game.getCurrentRun(games[0].id, { filter: games[0].id });
+
+            return game.getCurrentRun({ filter: games[0].id });
         };
 
         var userNotInGame = function (error) {
+            // is this possible?
             throw new Error('The current user is not in any game', error);
         };
 
