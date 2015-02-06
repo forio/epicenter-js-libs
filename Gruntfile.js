bcc220f3e757630067e8cb1745bdcb1d1c50ca47
Naren Ranjit <narendran.ranjit@gmail.com>
2015-02-06 12:59:37 -0800

    Also generating debug build of the cometd libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,29 +59,40 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     var BASE_COMETD_PATH = 'vendor/cometd-jquery/cometd-javascript/common/src/main/js/org/cometd/';
     var COMETD_PLUGINS_PATH = 'vendor/cometd-jquery/cometd-javascript/jquery/src/main/webapp/jquery';
+    var files = [
+        BASE_COMETD_PATH + '/cometd-header.js',
+        BASE_COMETD_PATH + '/cometd-namespace.js',
+        BASE_COMETD_PATH + '/cometd-json.js',
+        BASE_COMETD_PATH + '/Utils.js',
+        BASE_COMETD_PATH + '/TransportRegistry.js',
+        BASE_COMETD_PATH + '/Transport.js',
+        BASE_COMETD_PATH + '/RequestTransport.js',
+        BASE_COMETD_PATH + '/LongPollingTransport.js',
+        BASE_COMETD_PATH + '/CallbackPollingTransport.js',
+        BASE_COMETD_PATH + '/WebSocketTransport.js',
+        BASE_COMETD_PATH + '/Cometd.js',
+        COMETD_PLUGINS_PATH + '/jquery.cometd.js'
+    ];
     grunt.config.set('uglify', {
-        cometd: {
+        cometdMin: {
             options: {
                 sourceMap: false,
                 mangle: true,
                 beautify: false,
-                compress: true
+                compress: true,
             },
             files: {
-                'dist/epicenter-multiplayer-dependencies.js': [
-                    BASE_COMETD_PATH + '/cometd-header.js',
-                    BASE_COMETD_PATH + '/cometd-namespace.js',
-                    BASE_COMETD_PATH + '/cometd-json.js',
-                    BASE_COMETD_PATH + '/Utils.js',
-                    BASE_COMETD_PATH + '/TransportRegistry.js',
-                    BASE_COMETD_PATH + '/Transport.js',
-                    BASE_COMETD_PATH + '/RequestTransport.js',
-                    BASE_COMETD_PATH + '/LongPollingTransport.js',
-                    BASE_COMETD_PATH + '/CallbackPollingTransport.js',
-                    BASE_COMETD_PATH + '/WebSocketTransport.js',
-                    BASE_COMETD_PATH + '/Cometd.js',
-                    COMETD_PLUGINS_PATH + '/jquery.cometd.js'
-                ]
+                'dist/epicenter-multiplayer-dependencies.min.js': files
+            }
+        },
+        cometdDebug: {
+            options: {
+                sourceMap: true,
+                mangle: false,
+                compress: false,
+            },
+            files: {
+                'dist/epicenter-multiplayer-dependencies.js': files
             }
         }
     });
@@ -205,7 +216,8 @@ module.exports = function (grunt) {
     grunt.registerTask('test', ['browserify2:edge', 'mocha']);
     grunt.registerTask('documentation', ['markdox']);
     grunt.registerTask('validate', ['jshint:all', 'jscs', 'test']);
-    grunt.registerTask('production', ['uglify:cometd', 'validate', 'browserify2:mapped', 'browserify2:min', 'documentation']);
+    grunt.registerTask('concatCometd', ['uglify:cometdMin', 'uglify:cometdDebug']);
+    grunt.registerTask('production', ['concatCometd', 'validate', 'browserify2:mapped', 'browserify2:min', 'documentation']);
 
     grunt.registerTask('release', function (type) {
         //TODO: Integrate 'changelog' in here when it's stable
@@ -215,5 +227,5 @@ module.exports = function (grunt) {
         });
     });
 
-    grunt.registerTask('default', ['uglify:cometd', 'browserify2:edge', 'mocha:test', 'watch']);
+    grunt.registerTask('default', ['concatCometd', 'browserify2:edge', 'mocha:test', 'watch']);
 };
