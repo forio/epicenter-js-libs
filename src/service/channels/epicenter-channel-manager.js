eb0f6382ec9dc955873e3f34d4f911ba75e48d5b
Naren Ranjit <narendran.ranjit@gmail.com>
2015-02-09 15:44:03 -0800

    can subscribe to group channels

--- src/service/channels/epicenter-channel-manager.js
+++ src/service/channels/epicenter-channel-manager.js
@@ -8,30 +8,47 @@ var ChannelManager = require('./channel-manager');
 var classFrom = require('../../util/inherit');
 var urlService = require('../url-config-service');
 
+var AuthManager = require('../../managers/auth-manager');
 
 var __super = ChannelManager.prototype;
 
 var EpicenterChannelManager = classFrom(ChannelManager, {
     constructor: function (options) {
+        this.session = new AuthManager();
+        var userInfo = this.session.getCurrentUserSessionInfo();
+
         var defaults = {
             //See docs for url config service
-            server: {}
+            server: {
+                account: userInfo.account,
+                project: userInfo.project
+            }
         };
-        var defaultCometOptions = $.extend(true, {}, defaults, options);
+        var defaultCometOptions = $.extend(true, {}, defaults, userInfo, options);
 
         var urlOpts = urlService(defaultCometOptions.server);
         if (!defaultCometOptions.url) {
-            defaultCometOptions.url = urlOpts.protocol + '://' + urlOpts.host + '/channel/subscribe/project/';
+            defaultCometOptions.url = urlOpts.protocol + '://' + urlOpts.host + '/channel/subscribe';
         }
 
-        __super.constructor.call(this, defaultCometOptions);
+        this.options = defaultCometOptions;
+        return __super.constructor.call(this, defaultCometOptions);
     },
 
     getGameChannel: function () {
 
     },
-    getGroupChannel: function () {
+    getGroupChannel: function (groupName) {
+        if (!groupName) {
+            if (this.options.groupName) {
+                groupName = this.options.groupName;
+            } else {
+                throw new Error('Please specify a group name');
+            }
+        }
 
+        var baseTopic = '/project/' + this.options.server.account + '/' + this.options.server.project + '/' + groupName;
+        return __super.getChannel.call(this, { name: baseTopic });
     },
     getUserChannel: function () {
 
