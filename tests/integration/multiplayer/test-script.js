27d86cc9b7a3be1a0296c41ea19bb945ddf6f80d
Naren Ranjit <narendran.ranjit@gmail.com>
2015-02-10 11:53:40 -0800

    draft login page

--- tests/integration/multiplayer/test-script.js
+++ tests/integration/multiplayer/test-script.js
@@ -1,11 +1,36 @@
 'use strict';
 
 $(function () {
-   var cm = new F.manager.Channel({ server: { host: 'api.forio.com' } });
+    var server = { server: { host: 'api.forio.com' } };
+
+   var cm = new F.manager.Channel(server);
    cm.on('connect', function () {
        $('#status').html('connected');
    });
    cm.on('disconnect', function () {
        $('#status').html('disconnected');
+   });
+
+
+   var am = new F.manager.AuthManager(server);
+
+   $('#btnLogin').click(function (evt) {
+        evt.preventDefault();
+        am.login({
+            userName: $('#txtUsername').val(),
+            password: $('#txtPassword').val(),
+            account: $('#txtAccount').val(),
+            project: $('#txtProject').val()
+        }).done(function () {
+            window.alert('login successful');
+        });
+   });
+
+   $('#logout').click(function (evt) {
+       evt.preventDefault();
+
+       am.logout().then(function () {
+           window.alert('logged out');
+       });
    });
 });
