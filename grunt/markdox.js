86fa792969600ae29daf1bd315ca79c974d3c9c1
Molly Jones <mjones@forio.com>
2015-02-23 13:35:05 -0800

    add world-api-adapter.js to markdox for grunt

--- grunt/markdox.js
+++ grunt/markdox.js
@@ -25,6 +25,9 @@ module.exports = function (grunt) {
                     src: 'src/service/variables-api-service.js',
                     dest: 'documentation/generated/variables-api-service/index.html.md'
                 }, {
+                    src: 'src/service/world-api-adapter.js',
+                    dest: 'documentation/generated/world-api-adapter/index.html.md'
+                }, {
                     src: 'src/managers/run-manager.js',
                     dest: 'documentation/generated/run-manager/index.html.md'
                 }
