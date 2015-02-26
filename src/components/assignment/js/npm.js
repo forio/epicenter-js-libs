c78e611b56f7bd6b6ccb0681e138cfc98842e1d4
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-02-25 17:16:19 -0800

    Allow assignment.js to be at the top of the html

--- src/components/assignment/js/npm.js
+++ src/components/assignment/js/npm.js
@@ -1,3 +1,5 @@
-var App = require('./assignment.js');
-
-new App({ el: '#assignment-component' }).load();
+$(function () {
+    'use strict';
+    var App = require('./assignment.js');
+    new App({ el: '#assignment-component' }).load();
+});
