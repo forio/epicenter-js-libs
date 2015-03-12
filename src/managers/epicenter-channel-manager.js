65fb6ed05837f11cacbf136d62822713472e8f17
Molly Jones <mjones@forio.com>
2015-03-12 09:46:45 -0700

    epicenter-466 draft channel manager and service docs
    for more specific commit progress, see branch
    “epicenter-466-epijs-channel” in Epicenter project.

--- src/managers/epicenter-channel-manager.js
+++ src/managers/epicenter-channel-manager.js
@@ -1,7 +1,27 @@
 'use strict';
 
 /**
- * Wrapper around channel manager to instantiate it with epicenter-specific defaults
+ * ## Epicenter Channel Manager
+ *
+ * The Epicenter platform provides a push channel, which allows you to publish and subscribe to messages within a [project](../../../glossary/#projects), [group](../../../glossary/#groups), or [multiplayer world](../../../glossary/#world). There are two main use cases for the channel: event notifications and chat messages.
+ *
+ * The Epicenter Channel Manager is a wrapper around the (more generic) [Channel Manager](../channel-manager/), to instantiate it with Epicenter-specific defaults. If you are interested in including a notification or chat feature in your project, using an Epicenter Channel Manager is probably the easiest way to get started.
+ *
+ * You'll need to include the `epicenter-multiplayer-dependencies.js` library in addition to the `epicenter.js` library in your project to use the Epicenter Channel Manager. (See [Including Epicenter.js](../../#include).)
+ *
+ * To use the Epicenter Channel Manager: instantiate it, get the channel of the scope you want ([user](../../../glossary/#users), [world](../../../glossary/#world), or [group](../../../glossary/#groups)), then use the channel's `subscribe()` and `publish()` methods to subscribe to topics or publish data to topics.
+ *
+ *     var cm = new F.manager.EpicenterChannelManager();
+ *     var gc = cm.getGroupChannel();
+ *     gc.subscribe('broadcasts', callback);
+ *
+ * For additional background on Epicenter's push channel, see the introductory notes on the [Push Channel API](../../../rest_apis/multiplayer/channel/) page.
+ *
+ * The parameters for instantiating an Epicenter Channel Manager include:
+ *
+ * * `server` Object with details about the Epicenter project for this Epicenter Channel Manager instance.
+ * * `server.account` The Epicenter account id (**Team ID** for team projects, **User ID** for personal projects).
+ * * `server.project` Epicenter project id.
  */
 
 var ChannelManager = require('./channel-manager');
@@ -47,15 +67,23 @@ var EpicenterChannelManager = classFrom(ChannelManager, {
     },
 
     /**
-     * Get a pub/sub channel for the given group. There will be no default notifications from Epicenter on this channel, all messages will be user-orginated
+     * Create and return a publish/subscribe channel (from the underlying [Channel Manager](../channel-manager/)) for the given [group](../../../glossary/#groups). The group must be one of the groups in this account (team) and project.
      *
-     * ** Example usage **
-     *     var cm = new F.manager.Channel();
-     *     var gc = cm.getgetGroupChannel();
+     * There are no notifications from Epicenter on this channel; all messages are user-orginated.
+     *
+     * **Example**
+     *
+     *     var cm = new F.manager.EpicenterChannelManager();
+     *     var gc = cm.getGroupChannel();
      *     gc.subscribe('broadcasts', callback);
      *
-     * @param  {String} groupName (optional) Group to broadcast to. If not provided, picks up group from current session if logged in.
-     * @return {Channel}
+     * **Return Value**
+     *
+     * * *Channel* Returns the channel (an instance of the [Channel Service](../channel-service/)).
+     *
+     * **Parameters**
+     *
+     * @param  {String} `groupName` (Optional) Group to broadcast to. If not provided, picks up group from current session if end user is logged in.
      */
     getGroupChannel: function (groupName) {
         groupName = getFromSessionOrError(groupName, 'groupName');
@@ -64,13 +92,16 @@ var EpicenterChannelManager = classFrom(ChannelManager, {
     },
 
     /**
-     * Get a pub/sub channel for the given world. Note: You'll not get notifications for worlds which're not in memory. Typically used with the WorldManager
+     * Create and return a publish/subscribe channel (from the underlying [Channel Manager](../channel-manager/)) for the given [world](../../../glossary/#world).
      *
-     * ** Example usage **
-     *     var cm = new F.manager.Channel();
+     * This is typically used together with the [World Manager](../world-manager). Note that this channel only gets notifications for worlds currently in memory. (See more background on [persistence](../../../run_persistence).)
+     *
+     * **Example**
+     *
+     *     var cm = new F.manager.EpicenterChannelManager();
      *     var worldManager = new F.manager.WorldManager({
-     *         account: 'X',
-     *         project: 'Y',
+     *         account: 'acme-simulations',
+     *         project: 'supply-chain-game',
      *         model: 'model.eqn'
      *     });
      *     worldManager.getCurrentWorld().then(function (worldObject, worldService) {
@@ -80,9 +111,14 @@ var EpicenterChannelManager = classFrom(ChannelManager, {
      *         });
      *      });
      *
-     * @param  {String|Object} world     world object or id
-     * @param  {String} groupName (optional) Group the world exists in. If not provided, picks up group from current session if logged in.
-     * @return {Channel}
+     * **Return Value**
+     *
+     * * *Channel* Returns the channel (an instance of the [Channel Service](../channel-service/)).
+     *
+     * **Parameters**
+     *
+     * @param  {String|Object} `world` The world object or id.
+     * @param  {String} `groupName` (Optional) Group the world exists in. If not provided, picks up group from current session if end user is logged in.
      */
     getWorldChannel: function (world, groupName) {
         var worldid = ($.isPlainObject(world) && world.id) ? world.id : world;
@@ -95,26 +131,35 @@ var EpicenterChannelManager = classFrom(ChannelManager, {
     },
 
     /**
-     * Get a pub/sub channel for the current user. Needs the user's current world to work. Note: You'll not get notifications for worlds which're not in memory. Typically used with the WorldManager
+     * Create and return a publish/subscribe channel (from the underlying [Channel Manager](../channel-manager/)) for the current [end user](../../../glossary/#user) in that user's current [world](../../../glossary/#world).
      *
-     * ** Example usage **
-     *     var cm = new F.manager.Channel();
+     * This is typically used together with the [World Manager](../world-manager). Note that this channel only gets notifications for worlds currently in memory. (See more background on [persistence](../../../run_persistence).)
+     *
+     * **Example**
+     *
+     *     var cm = new F.manager.EpicenterChannelManager();
      *     var worldManager = new F.manager.WorldManager({
-     *         account: 'X',
-     *         project: 'Y',
+     *         account: 'acme-simulations',
+     *         project: 'supply-chain-game',
      *         model: 'model.eqn'
      *     });
      *     worldManager.getCurrentWorld().then(function (worldObject, worldService) {
-     *         var worldChannel = cm.getWorldChannel(worldObject);
-     *         worldChannel.subscribe('', function (data) {
+     *         var userChannel = cm.getUserChannel(worldObject);
+     *         userChannel.subscribe('', function (data) {
      *             console.log(data);
      *         });
      *      });
      *
-     * @param  {String|Object} world     world object or id
-     * @param  {String|Object} user (Optional)    user object or id. If not provided, picks up user id from current session if logged in.
-     * @param  {String} groupName (optional) Group the world exists in. If not provided, picks up group from current session if logged in.
-     * @return {Channel}
+     *
+     * **Return Value**
+     *
+     * * *Channel* Returns the channel (an instance of the [Channel Service](../channel-service/)).
+     *
+     * **Parameters**
+     *
+     * @param  {String|Object} `world` World object or id.
+     * @param  {String|Object} `user` (Optional) User object or id. If not provided, picks up ser id from current session if end user is logged in.
+     * @param  {String} `groupName` (Optional) Group the world exists in. If not provided, picks up group from current session if end user is logged in.
      */
     getUserChannel: function (world, user, groupName) {
         var userid = ($.isPlainObject(user) && user.id) ? user.id : user;
--- src/service/channel-service.js
+++ src/service/channel-service.js
@@ -1,26 +1,54 @@
 'use strict';
 
 /**
- * A 'pub/sub' object
- * @param {Object} options
- * @param {String} options.base Base-topic. This will be pre-pended to all further topics you publish or subscribe to
- * @param {Function} topicResolver A function into which all 'topics' passed into the `publish` and `subscribe` methods are sent through. By default it just echoes.
- * @param {Cometd} transport instance of cometd to hook onto
+ * ## Channel Service
+ *
+ * The Epicenter platform provides a push channel, which allows you to publish and subscribe to messages within a [project](../../../glossary/#projects), [group](../../../glossary/#groups), or [multiplayer world](../../../glossary/#world). There are two main use cases for the channel: event notifications and chat messages.
+ *
+ * The Channel Service is a building block for this functionality. It creates a publish-subscribe object, allowing you to publish messages, subscribe to messages, or unsubscribe from messages for a given 'topic' on a `$.cometd` transport instance.
+ *
+ * Typically, you use the [Epicenter Channel Manager](../epicenter-channel-manager/) to create or retrieve channels, then use the Channel Service `subscribe()` and `publish()` methods to listen to or update data. (For additional background on Epicenter's push channel, see the introductory notes on the [Push Channel API](../../../rest_apis/multiplayer/channel/) page.)
+ *
+ * You'll need to include the `epicenter-multiplayer-dependencies.js` library in addition to the `epicenter.js` library in your project to use the Channel Service. (See [Including Epicenter.js](../../#include).)
+ *
+ * To use the Channel Service, instantiate it, then make calls to any of the methods you need.
+ *
+ *        var cs = new F.service.Channel();
+ *        cs.publish('run/variables', {price: 50});
+ *
+ * The parameters for instantiating a Channel Service include:
+ *
+ * * `options` The options object to configure the Channel Service.
+ * * `options.base` The base topic. This is added as a prefix to all further topics you publish or subscribe to while working with this Channel Service.
+ * * `options.topicResolver` A function that processes all 'topics' passed into the `publish` and `subscribe` methods. This is useful if you want to implement your own serialize functions for converting custom objects to topic names. Returns a String. By default, it just echoes the topic.
+ * * `options.transport` The instance of `$.cometd` to hook onto. See http://docs.cometd.org/reference/javascript.html for additional background on cometd.
  */
 var Channel = function (options) {
     var defaults = {
+
+        /**
+         * The base topic. This is added as a prefix to all further topics you publish or subscribe to while working with this Channel Service.
+         */
         base: '',
 
         /**
-         * This is useful if you want to implement your own serialize functions for converting custom objects to topic names.
-         * @param  {*} token Token to parse
-         * @return {String} this function should return a string topic
+         * A function that processes all 'topics' passed into the `publish` and `subscribe` methods. This is useful if you want to implement your own serialize functions for converting custom objects to topic names. By default, it just echoes the topic.
+         *
+         * **Parameters**
+         *
+         * * `topic` Topic to parse.
+         *
+         * **Return Value**
+         *
+         * * *String*: This function should return a string topic.
          */
         topicResolver: function (topic) {
             return topic;
         },
 
-        //cometd instance
+        /**
+         * The instance of `$.cometd` to hook onto.
+         */
         transport: null
     };
     this.channelOptions = $.extend(true, {}, defaults, options);
@@ -32,81 +60,122 @@ var makeName = function (channelName, topic) {
     return newName;
 };
 
-/**
- * Subscribe to changes on a topic.
- *
- * @param  {String | Array}   topic    List of topics to listen for changes on
- * @param  {Function} callback Callback to execute. Callback will be called with signature (evt, payload, metadata)
- * @param  {Object}   context  Context in which the callback will be executed
- * @param  {Object}   options  Object with defaults
- * @param  {Number}   options.priority  Can be used to control order of operations, defaults to 0, can be any +ve or -ve number
- * @param  {String|Number|Function}   options.value  Callback will only be triggered if this condition matches. See Examples for details
- * @return {string}            Returns a token you can later use to un-subscribe
- *
- *  **Examples**
- *  subscribe('run', cb) - subscribe to changes on a top-level 'run' topic
- *  subscribe('run/*') - changes on children of the run object. Note this not also be triggered for changes to run.x.y.z
- *  subscribe(['run', 'run/*']) - changes to run and children
- *  subscribe('run/variables/price', cb) - changes to price
- *
- *  subscribe('run', function () { this.innerHTML = 'Triggered'}, document.body); - set the context for the callback
- *  subscribe('run', cb, this, {priority: 9}); - by default priority is 0. Can be used to control order of operations
- *  subscribe('run/variables/price', cb, this, {priority: 30, value: 50}); - only exec callback if value of price is 50
- *  subscribe('run/variables/price', cb, this, {priority: 30, value: '>50'}); - only exec callback if value of price is > 50
- *  subscribe('run/variables/price', cb, this, {priority: 30, value: function (val) {return val % 2 ===0}}); - only exec callback if value of price is even
- *
- */
-Channel.prototype.subscribe = function (topic, callback, context, options) {
-    var topics = [].concat(topic);
-    var me = this;
-    var subscriptionIds = [];
-    var opts = me.channelOptions;
 
-    opts.transport.batch(function () {
-        $.each(topics, function (index, topic) {
-            topic = makeName(opts.base, opts.topicResolver(topic));
-            subscriptionIds.push(opts.transport.subscribe(topic, callback));
+Channel.prototype = $.extend(Channel.prototype, {
+
+    /**
+     * Subscribe to changes on a topic.
+     *
+     *  **Examples**
+     *
+     *      // Subscribe to changes on a top-level 'run' topic
+     *      cs.subscribe('run', cb);
+     *
+     *      // Subscribe to changes on children of the 'run' topic. Note this not also be triggered for changes to run.x.y.z.
+     *      cs.subscribe('run/*');
+     *
+     *      // Subscribe to changes on both the top-level 'run' topic and its children
+     *      cs.subscribe(['run', 'run/*']);
+     *
+     *      // Subscribe to changes on a particular variable
+     *      subscribe('run/variables/price', cb);
+     *
+     *      // Set the context for the callback
+     *      cs.subscribe('run', function () { this.innerHTML = 'Triggered'}, document.body);
+     *
+     *      // Control the order of operations by setting the `priority`
+     *      cs.subscribe('run', cb, this, {priority: 9});
+     *
+     *      // Only execute the callback, `cb`, if the value of the `price` variable is 50
+     *      cs.subscribe('run/variables/price', cb, this, {priority: 30, value: 50});
+     *
+     *      // Only execute the callback, `cb`, if the value of the `price` variable is greater than 50
+     *      subscribe('run/variables/price', cb, this, {priority: 30, value: '>50'});
+     *
+     *      // Only execute the callback, `cb`, if the value of the `price` variable is even
+     *      subscribe('run/variables/price', cb, this, {priority: 30, value: function (val) {return val % 2 === 0}});
+     *
+     * **Return Value**
+     *
+     * * *String* Returns a token you can later use to unsubscribe.
+     *
+     * **Parameters**
+     * @param  {String|Array}   `topic`    List of topics to listen for changes on.
+     * @param  {Function} `callback` Callback function to execute. Callback is called with signature `(evt, payload, metadata)`.
+     * @param  {Object}   `context`  Context in which the `callback` is executed.
+     * @param  {Object}   `options`  (Optional) Overrides for configuration options.
+     * @param  {Number}   `options.priority`  Used to control order of operations. Defaults to 0. Can be any +ve or -ve number.
+     * @param  {String|Number|Function}   `options.value` The `callback` is only triggered if this condition matches. See examples for details.
+     *
+     */
+    subscribe: function (topic, callback, context, options) {
+
+        var topics = [].concat(topic);
+        var me = this;
+        var subscriptionIds = [];
+        var opts = me.channelOptions;
+
+        opts.transport.batch(function () {
+            $.each(topics, function (index, topic) {
+                topic = makeName(opts.base, opts.topicResolver(topic));
+                subscriptionIds.push(opts.transport.subscribe(topic, callback));
+            });
         });
-    });
-    return (subscriptionIds[1] ? subscriptionIds : subscriptionIds[0]);
-};
+        return (subscriptionIds[1] ? subscriptionIds : subscriptionIds[0]);
+    },
 
-/**
- * Publish data to a topic
- * @param  {String} topic Topic to publish to
- * @param  {*} payload  Data to publish to topic
- *
- * **Examples**
- * publish('run', {completed: false}); - send payload to all subscribers
- * publish('run/variables', {price: 50});
- */
-Channel.prototype.publish = function (topic, data) {
-    var topics = [].concat(topic);
-    var me = this;
-    var returnObjs = [];
-    var opts = me.channelOptions;
+    /**
+     * Publish data to a topic.
+     *
+     * **Examples**
+     *
+     *      // Send data to all subscribers of the 'run' topic
+     *      cs.publish('run', {completed: false});
+     *
+     *      // Send data to all subscribers of the 'run/variables' topic
+     *      cs.publish('run/variables', {price: 50});
+     *
+     * **Parameters**
+     *
+     * @param  {String} `topic` Topic to publish to.
+     * @param  {*} `payload`  Data to publish to topic.
+     *
+     */
+    publish: function (topic, data) {
+        var topics = [].concat(topic);
+        var me = this;
+        var returnObjs = [];
+        var opts = me.channelOptions;
 
 
-    opts.transport.batch(function () {
-        $.each(topics, function (index, topic) {
-            topic = makeName(opts.base, opts.topicResolver(topic));
-            if (topic.charAt(topic.length - 1) === '*') {
-                topic = topic.replace(/\*+$/, '');
-                console.warn('You can cannot publish to channels with wildcards. Publishing to ', topic, 'instead');
-            }
-            returnObjs.push(opts.transport.publish(topic, data));
+        opts.transport.batch(function () {
+            $.each(topics, function (index, topic) {
+                topic = makeName(opts.base, opts.topicResolver(topic));
+                if (topic.charAt(topic.length - 1) === '*') {
+                    topic = topic.replace(/\*+$/, '');
+                    console.warn('You can cannot publish to channels with wildcards. Publishing to ', topic, 'instead');
+                }
+                returnObjs.push(opts.transport.publish(topic, data));
+            });
         });
-    });
-    return (returnObjs[1] ? returnObjs : returnObjs[0]);
-};
+        return (returnObjs[1] ? returnObjs : returnObjs[0]);
+    },
 
-/**
- * Unsubscribe from changes
- * @param  {String} token
- */
-Channel.prototype.unsubscribe = function (token) {
-    this.channelOptions.transport.unsubscribe(token);
-    return token;
-};
+    /**
+     * Unsubscribe from changes to a topic.
+     *
+     * **Example**
+     *
+     *      cs.unsubscribe('sampleToken');
+     *
+     * **Parameters**
+     * @param  {String} `token` The token for topic is returned when you initially subscribe. Pass it here to unsubscribe from that topic.
+     */
+    unsubscribe: function (token) {
+        this.channelOptions.transport.unsubscribe(token);
+        return token;
+    }
+
+});
 
 module.exports = Channel;
