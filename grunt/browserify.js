c31445e4835557dd83e5f01bf824030d6f3c201a
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-03-17 11:00:12 -0700

    Renamed entry point for assignment component js file

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
         },
         components: {
             files: {
-                './dist/components/assignment/assignment.js': './src/components/assignment/js/npm.js'
+                './dist/components/assignment/assignment.js': './src/components/assignment/js/index.js'
             }
         }
 
