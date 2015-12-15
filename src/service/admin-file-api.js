c28578cc8bea059ed5b8fcbda6a3b99388e31e00
Naren Ranjit <narendran.ranjit@gmail.com>
2015-12-14 17:10:08 -0800

    manually send header for admin file api

--- src/service/admin-file-api.js
+++ src/service/admin-file-api.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
          * @see [Authentication API Service](../auth-api-service/) for getting tokens.
          * @type {String}
          */
-        token: store.get('epicenter.project.token') || '',
+         token: store.get('epicenter.project.token') || store.get('epicenter.token') || '',
 
         /**
          * The account id. In the Epicenter UI, this is the **Team ID** (for team projects) or **User ID** (for personal projects). Defaults to empty string.
