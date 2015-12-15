279511db415136dd7944758e1d487bd3177b8beb
Naren Ranjit <narendran.ranjit@gmail.com>
2015-12-14 17:07:54 -0800

    added admin file api, expose run cnfig

---
+++ src/service/admin-file-api.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var ConfigService = require('./configuration-service');
+var StorageFactory = require('../store/store-factory');
+var TransportFactory = require('../transport/http-transport-factory');
+
+module.exports = function (config) {
+    // config || (config = configService.get());
+    var store = new StorageFactory({ synchronous: true });
+
+    var defaults = {
+        /**
+         * For projects that require authentication, pass in the user access token (defaults to empty string). If the user is already logged in to Epicenter, the user access token is already set in a cookie and automatically loaded from there. (See [more background on access tokens](../../../project_access/)).
+         * @see [Authentication API Service](../auth-api-service/) for getting tokens.
+         * @type {String}
+         */
+        token: store.get('epicenter.project.token') || '',
+
+        /**
+         * The account id. In the Epicenter UI, this is the **Team ID** (for team projects) or **User ID** (for personal projects). Defaults to empty string.
+         * @type {String}
+         */
+        account: '',
+
+        /**
+         * The project id. Defaults to empty string.
+         * @type {String}
+         */
+        project: '',
+
+
+        /**
+         * Options to pass on to the underlying transport layer. All jquery.ajax options at http://api.jquery.com/jQuery.ajax/ are available. Defaults to empty object.
+         * @type {Object}
+         */
+        transport: {}
+    };
+
+    var serviceOptions = $.extend({}, defaults, config);
+    var urlConfig = new ConfigService(serviceOptions).get('server');
+    if (serviceOptions.account) {
+        urlConfig.accountPath = serviceOptions.account;
+    }
+    if (serviceOptions.project) {
+        urlConfig.projectPath = serviceOptions.project;
+    }
+
+    var httpOptions = $.extend(true, {}, serviceOptions.transport, {
+        url: urlConfig.getAPIPath('file')
+    });
+
+    if (serviceOptions.token) {
+        httpOptions.headers = {
+            'Authorization': 'Bearer ' + serviceOptions.token
+        };
+    }
+    var http = new TransportFactory(httpOptions);
+
+
+    var publicAsyncAPI = {
+        getFileContents: function (fileName, folder, options) {
+            var path = folder + '/' + fileName;
+            var httpOptions = $.extend(true, {}, serviceOptions, options, {
+                url: urlConfig.getAPIPath('file') + path
+            });
+            return http.get('', httpOptions);
+        }
+    };
+
+    $.extend(this, publicAsyncAPI);
+};
