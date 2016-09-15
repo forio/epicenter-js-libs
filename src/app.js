71c16f815a334194d92f3d361a135446b0c962bf
Naren Ranjit <narendran.ranjit@gmail.com>
2016-09-15 12:15:57 -0700

    expose introspection as a service

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ F.service.State = require('./service/state-api-adapter');
 F.service.User = require('./service/user-api-adapter');
 F.service.Member = require('./service/member-api-adapter');
 F.service.Asset = require('./service/asset-api-adapter');
+F.service.Introspect = require('./service/introspection-api-service');
 
 F.store.Cookie = require('./store/cookie-store');
 F.factory.Store = require('./store/store-factory');
