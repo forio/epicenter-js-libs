8943de1e3d37efb48c57d97610b12d2866509f1b
Naren Ranjit <narendran.ranjit@gmail.com>
2016-12-01 10:10:28 -0800

    remove dependency on auth manager and session store

--- src/managers/run-strategies/persistent-single-player-strategy.js
+++ src/managers/run-strategies/persistent-single-player-strategy.js
@@ -15,33 +15,21 @@
 
 var classFrom = require('../../util/inherit');
 var IdentityStrategy = require('./none-strategy');
-var StorageFactory = require('../../store/store-factory');
 var StateApi = require('../../service/state-api-adapter');
-var AuthManager = require('../auth-manager');
 
-var keyNames = require('../key-names');
-
-var defaults = {
-    store: {
-        synchronous: true
-    }
-};
+var defaults = {};
 
 var Strategy = classFrom(IdentityStrategy, {
     constructor: function Strategy(options) {
         this.options = $.extend(true, {}, defaults, options);
-        this.runOptions = this.options.run;
-        this._store = new StorageFactory(this.options.store);
         this.stateApi = new StateApi();
-        this._auth = new AuthManager();
     },
 
-    reset: function (runService) {
-        var session = this._auth.getCurrentUserSessionInfo();
+    reset: function (runService, userSession) {
+        var group = userSession && userSession.groupName;
         var opt = $.extend({
-            scope: { group: session.groupName }
-        }, this.runOptions);
-
+            scope: { group: group }
+        }, runService.getCurrentConfig());
         return runService
             .create(opt)
             .then(function (run) {
@@ -50,12 +38,14 @@ var Strategy = classFrom(IdentityStrategy, {
             });
     },
 
-    getRun: function (runService) {
+    getRun: function (runService, userSession) {
+        if (!userSession || userSession === {}) {
+            return $.Deferred().reject('No user-session provided. Persistent single-player strategy needs a logged-in user.').promise();
+        }
         var me = this;
-        var session = JSON.parse(this._store.get(keyNames.EPI_SESSION_KEY) || '{}');
         return runService.query({
-            'user.id': session.userId || '0000',
-            'scope.group': session.groupName
+            'user.id': userSession.userId || '0000',
+            'scope.group': userSession.groupName
         }).then(function (runs) {
             return me._loadAndCheck(runService, runs);
         });
