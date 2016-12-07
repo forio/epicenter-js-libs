047d3e3d04abb08e3e21bb6a11a17ac2d88cd3ae
Naren Ranjit <narendran.ranjit@gmail.com>
2016-12-07 14:05:58 -0800

    failing scen tests

--- src/managers/scenario-manager.js
+++ src/managers/scenario-manager.js
@@ -5,35 +5,32 @@ var SessionManager = require('../store/session-manager');
 var SavedRunsManager = require('./saved-run-manager');
 
 var defaults = {
-    token: undefined,
-    account: undefined,
-    project: undefined,
-    model: undefined,
-
     baselineRunName: 'Baseline',
 };
 
 function ScenarioManager(config) {
     var sessionManager = new SessionManager();
-    this.serviceOptions = sessionManager.getMergedOptions(defaults, config);
+    var serviceOptions = sessionManager.getMergedOptions(defaults, config);
     this.baseline = new RunManager({
         strategy: 'baseline',
         sessionKey: 'sm-baseline-run',
-        run: this.serviceOptions,
+        run: serviceOptions,
     });
     this.current = new RunManager({
         strategy: 'new-if-stepped',
         sessionKey: 'sm-current-run',
-        run: this.serviceOptions,
+        run: serviceOptions,
     });
 
     var baseLineProm = this.baseline.getRun();
 
-    this.savedRuns = new SavedRunsManager(this.serviceOptions);
+    this.savedRuns = new SavedRunsManager({
+        run: serviceOptions,
+    });
 
     var orig = this.savedRuns.getRuns;
     this.savedRuns.getRuns = function () {
-        var args = arguments;
+        var args = Array.apply(null, arguments);
         var me = this;
         return baseLineProm.then(function () {
             return orig.apply(me.savedRuns, args);
--- tests/spec/test-scenario-manager.js
+++ tests/spec/test-scenario-manager.js
@@ -1,16 +1,59 @@
 (function () {
     'use strict';
 
+    var ScenarioManager = F.manager.ScenarioManager;
+    var RunManager = F.manager.RunManager;
+    var SavedRunsManager = F.manager.SavedRunsManager;
+
     var runOptions = {
         model: 'model.eqn',
         account: 'forio-dev',
         project: 'js-libs'
     };
 
-    describe('Scenario Manager', function () {
+    var sampleSession = {
+        auth_token: '',
+        account: 'forio-dev',
+        project: 'js-libs',
+        userId: '123',
+        groupId: 'group123',
+        groupName: 'group-123',
+        isFac: false
+    };
+    var fakeAuth = {
+        // get should return what's stored in the session cookie
+        getCurrentUserSessionInfo: sinon.stub().returns(sampleSession)
+    };
+    var fakeInvalidAuth = {
+        // get should return what's stored in the session cookie
+        getCurrentUserSessionInfo: sinon.stub().returns({})
+    };
+
+    describe.only('Scenario Manager', function () {
         var server;
         beforeEach(function () {
             server = sinon.fakeServer.create();
+            server.respondWith('PATCH', /(.*)\/run\/(.*)\/(.*)/, function (xhr, id) {
+                xhr.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify({ url: xhr.url }));
+            });
+            server.respondWith('POST', /(.*)\/run\/(.*)\/(.*)/, function (xhr, id) {
+                var resp = {
+                    id: '065dfe50-d29d-4b55-a0fd-30868d7dd26c',
+                    model: 'model.vmf',
+                    account: 'mit',
+                    project: 'afv',
+                    saved: false,
+                    lastModified: '2014-06-20T04:09:45.738Z',
+                    created: '2014-06-20T04:09:45.738Z'
+                };
+                xhr.respond(201, { 'Content-Type': 'application/json' }, JSON.stringify(resp));
+            });
+
+            // General GET
+            server.respondWith('GET', /(.*)\/run\/(.*)\/(.*)/, function (xhr, id) {
+                xhr.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify({ url: xhr.url }));
+                return true;
+            });
             server.respondImmediately = true;
         });
 
@@ -18,43 +61,34 @@
             server.restore();
         });
 
-        describe('constructor', ()=> {
-            it('should create a new baseline run manager', ()=> {
-                
+        describe('baseline', function () {
+            it('should create a new baseline run manager', function () {
+                var sm = new ScenarioManager();
+                expect(sm.baseline).to.be.instanceof(RunManager);
             });
-            it('should create a new current run manager', ()=> {
-                
-            });
-            it('should create a new baseline run on instantiation', ()=> {
-                
+            it('should pass through the right options', function () {
+                var sm = new ScenarioManager(runOptions);
+                sm.baseline.authManager = fakeInvalidAuth;
+
+                return sm.baseline.getRun().then(function (run) {
+                    var config = sm.baseline.run.getCurrentConfig();
+                    expect(config.account).to.equal(runOptions.account);
+                    expect(config.project).to.equal(runOptions.project);
+                });
             });
         });
-        describe('#getSavedRuns', function () {
-            var runService;
-
-            beforeEach(function () {
-
+        describe('current run', function () {
+            it('should create a new current run manager', function () {
+                var sm = new ScenarioManager();
+                expect(sm.current).to.be.instanceof(RunManager);
             });
-
-            it('should query with the right params', function () {
-                
-            });
-            it('should allow over-riding modifiers', function () {
-                
-            });
-
-            describe('when variables are requested', function () {
-                it('should not get variables if no runs exist', function () {
-                    
-                });
-                it('should not get variables if not asked for', function () {
-                    
-                });
-                it('should query variables service if asked for variables', function () {
-                    
-                });
-            });
-
         });
+        describe('saved runs', function () {
+            it('should create a new saved run manager', function () {
+                var sm = new ScenarioManager();
+                expect(sm.savedRuns).to.be.instanceof(SavedRunsManager);
+            });
+        });
+       
     });
 }());
