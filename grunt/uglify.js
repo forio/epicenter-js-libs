5e6a2d6195bd2fa8fb283358bbd37f6fb6f39574
Naren Ranjit <narendran.ranjit@gmail.com>
2016-12-07 15:22:47 -0800

    updated build with extensions

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,7 +1,14 @@
 'use strict';
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    var files = ['../node_modules/cometd-jquery/org/cometd.js', '../node_modules/cometd-jquery/jquery/jquery.cometd.js'];
+    var files = [
+        '../node_modules/cometd-jquery/org/cometd.js', 
+        '../node_modules/cometd-jquery/org/cometd/AckExtension.js', 
+        '../node_modules/cometd-jquery/org/cometd/ReloadExtension.js', 
+        '../node_modules/cometd-jquery/jquery/jquery.cometd.js',
+        '../node_modules/cometd-jquery/jquery/jquery.cometd-ack.js',
+        '../node_modules/cometd-jquery/jquery/jquery.cometd-reload.js',
+    ];
     grunt.config.set('uglify', {
         options: {
             sourceMap: true,
