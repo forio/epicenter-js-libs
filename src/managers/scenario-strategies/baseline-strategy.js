2e7769c271dcfa855a8535356aaff9e544faead4
Naren <narendran.ranjit@gmail.com>
2017-01-10 08:24:46 -0800

    simplify baseline strategy

--- src/managers/scenario-strategies/baseline-strategy.js
+++ src/managers/scenario-strategies/baseline-strategy.js
@@ -1,53 +1,21 @@
 'use strict';
-var classFrom = require('../../util/inherit');
-var injectFiltersFromSession = require('../strategy-utils').injectFiltersFromSession;
-var injectScopeFromSession = require('../strategy-utils').injectScopeFromSession;
 
-var Base = {};
-var BASELINE_NAME = 'baseline';
+var ReuseinitStrategy = require('../run-strategies/reuse-last-initialized');
 
-//FIXME: Merge with reuse-last-initialized
-module.exports = classFrom(Base, {
-    constructor: function (runService, options) {
-    },
+module.exports = function (options) {
+    var defaults = {
+        baselineName: 'baseline'
+    };
+    var opts = $.extend(true, defaults, options);
 
-    reset: function (runService, userSession) {
-        var opt = injectScopeFromSession(runService.getCurrentConfig(), userSession);
-
-        return runService.create(opt).then(function (createResponse) {
-            return runService.save({
+    return new ReuseinitStrategy({
+        strategyOptions: {
+            initOperation: [{ name: 'stepTo', params: 'end' }],
+            flag: {
                 saved: true,
-                trashed: false, //TODO remove this once EPICENTER-2500 is fixed
-                name: BASELINE_NAME
-            }).then(function (patchResponse) {
-                return $.extend(true, {}, createResponse, patchResponse);
-            });
-        }).then(function (mergedResponse) {
-            //FIXME: Fix hard-coded 'stepto' assumption
-            return runService.do({ stepTo: 'end' }).then(function () {
-                return mergedResponse;
-            });
-        });
-    },
-
-    getRun: function (runService, userSession, runIdInSession) {
-        var filter = injectFiltersFromSession({ 
-            saved: true, 
-            trashed: false, //TODO remove this once EPICENTER-2500 is fixed
-            name: BASELINE_NAME,
-        }, userSession);
-
-        var me = this;
-        return runService.filter(filter, { 
-            // startrecord: 0, //TODO: Uncomment when EPICENTER-2569 is fixed
-            // endrecord: 0,
-            sort: 'created', 
-            direction: 'desc'
-        }).then(function (runs) {
-            if (!runs.length) {
-                return me.reset(runService, userSession);
+                trashed: false,
+                name: opts.baselineName
             }
-            return runs[0];
-        });
-    }
-}, { requiresAuth: false });
+        }
+    });
+};
