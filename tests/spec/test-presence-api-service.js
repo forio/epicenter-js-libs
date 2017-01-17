f985e127098ccfb7e8c0d422429a2d6034165d25
Tom Chen <senluchen2015@u.northwestern.edu>
2017-01-16 17:02:50 -0800

    presence service update test

--- tests/spec/test-presence-api-service.js
+++ tests/spec/test-presence-api-service.js
@@ -3,12 +3,6 @@
 
     describe('Presence API Service', function () {
         var server;
-        var cookieDummy = {
-            get: function () {
-                return '';
-            },
-            set: function (newCookie) {}
-        };
         before(function () {
             server = sinon.fakeServer.create();
             server.respondWith('POST', /(.*)\/game/, function (xhr, id) {
@@ -27,7 +21,7 @@
         }
 
         describe('markOnline', function () {
-            it('should call POST on the Presence API with the userId parameter and the token', function () {
+            it('should call POST on the Presence API with the userId parameter', function () {
                 createPresenceAdapter({ token: '123', groupName: 'test-group' })
                     .markOnline('abc123');
 
@@ -37,6 +31,17 @@
                 (/abc123/).test(req.url).should.be.true;
                 (/Bearer 123/).test(req.requestHeaders.Authorization).should.be.true;
             });
+
+            it('should call POST on the Presence API with the token parameter', function () {
+                createPresenceAdapter({ token: '123', groupName: 'test-group' })
+                    .markOnline('abc123');
+
+                var req = server.requests.pop();
+                req.method.toUpperCase().should.equal('POST');
+                (/\/presence\//).test(req.url).should.be.true;
+                (/Bearer 123/).test(req.requestHeaders.Authorization).should.be.true;
+            });
+
 
             it('should throw error when no userId is specified', function () {
                 var markOnline = function () {
@@ -55,6 +60,15 @@
                 req.method.toUpperCase().should.equal('DELETE');
                 (/\/presence\//).test(req.url).should.be.true;
                 (/abc123/).test(req.url).should.be.true;
+            });
+
+            it('should call Delete on the Presence API with the token', function () {
+                createPresenceAdapter({ token: '123', groupName: 'test-group' })
+                    .markOffline('abc123');
+
+                var req = server.requests.pop();
+                req.method.toUpperCase().should.equal('DELETE');
+                (/\/presence\//).test(req.url).should.be.true;
                 (/Bearer 123/).test(req.requestHeaders.Authorization).should.be.true;
             });
 
