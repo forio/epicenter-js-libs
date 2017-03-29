e901d12411994dc3b4108135fdcf614bd4f3c746
Molly Jones <mjones@forio.com>
2017-03-29 15:36:16 -0700

    epi-1516 minor edits after initial review

--- src/managers/epicenter-channel-manager.js
+++ src/managers/epicenter-channel-manager.js
@@ -3,7 +3,7 @@
 /**
  * ## Epicenter Channel Manager
  *
- * The Epicenter platform provides a push channel, which allows you to publish and subscribe to messages within a [project](../../../glossary/#projects), [group](../../../glossary/#groups), or [multiplayer world](../../../glossary/#world). There are two main use cases for the channel: event notifications and chat messages.
+ * The Epicenter platform provides a push channel, which allows you to publish and subscribe to messages within a [project](../../../glossary/#projects), [group](../../../glossary/#groups), or [multiplayer world](../../../glossary/#world).
  *
  * <a name="background"></a>
  * ### Channel Background
@@ -18,7 +18,7 @@
  *
  * Usually, this "something new" is an event within the project, group, or world, such as:
  *
- * * An end user comes online (logs in) or goes offline. (This is especially interesting in a multiplayer world; only available if you have enabled authorization for the channel.)
+ * * An end user comes online (logs in) or goes offline. (This is especially interesting in a multiplayer world; only available if you have [enabled authorization](../../../updating_your_settings/#general-settings) for the channel.)
  * * An end user is assigned to a world.
  * * An end user updates a variable / makes a decision.
  * * An end user creates or updates data stored in the [Data API](../data-api-service/).
@@ -40,7 +40,7 @@
  * * Next, get the channel with the scope you want (user, world, group, data).
  * * Finally, use the channel's `subscribe()` and `publish()` methods to subscribe to topics or publish data to topics.
  *
- * **Example**:
+ * Here's an example of those last three steps (instantiate, get channel, subscribe):
  *
  *     var cm = new F.manager.ChannelManager();
  *     var gc = cm.getGroupChannel();
@@ -52,7 +52,7 @@
  *
  * #### Creating an Epicenter Channel Manager
  *
- * The Epicenter Channel Manager is a wrapper around the (more generic) [Channel Manager](../channel-manager/), to instantiate it with Epicenter-specific defaults. If you are interested in including a notification or chat feature in your project, using an Epicenter Channel Manager is probably the easiest way to get started.
+ * The Epicenter Channel Manager is a wrapper around the (more generic) [Channel Manager](../channel-manager/), to instantiate it with Epicenter-specific defaults. If you are interested in including a notification or chat feature in your project, using an Epicenter Channel Manager is the easiest way to get started.
  *
  * You'll need to include the `epicenter-multiplayer-dependencies.js` library in addition to the `epicenter.js` library in your project to use the Epicenter Channel Manager. See [Including Epicenter.js](../../#include).
  *
