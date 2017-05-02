c84eb495f1ea4a1bf1e470dac088c3823fa336df
Molly Jones <mjones@forio.com>
2017-05-02 13:52:31 -0700

    epi-2776 remove logout.js (moves to function in auth.js)

--- src/components/login/logout.js
+++
@@ -1,8 +0,0 @@
-$('.header .logout').on('click', function (evt) {
-    var authManager = new F.manager.AuthManager();
-    authManager.logout().then(function (token) {
-        console.log('Logged out, redirecting to index.html');
-        authManager.sessionManager.getStore().remove('epicenter-scenario');
-        window.location.href = 'index.html';
-    });
-});
