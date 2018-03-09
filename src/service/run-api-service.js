fdd7cdec14b76cf17e5f190924c67a21da69c315
Naren Ranjit <narendran.ranjit@gmail.com>
2018-03-09 10:58:14 -0800

    EPICENTER-3453 support cinFiles

--- src/service/run-api-service.js
+++ src/service/run-api-service.js
@@ -217,7 +217,7 @@ module.exports = function (config) {
          */
         create: function (params, options) {
             var createOptions = $.extend(true, {}, serviceOptions, options, { url: urlConfig.getAPIPath('run') });
-            var runApiParams = ['model', 'scope', 'files', 'ephemeral'];
+            var runApiParams = ['model', 'scope', 'files', 'ephemeral', 'cinFiles'];
             if (typeof params === 'string') {
                 // this is just the model name
                 params = { model: params };
