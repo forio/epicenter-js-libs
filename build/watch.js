2ade9fa8a2e86a73ac4d21d6b6e08cecb7a03372
Naren Ranjit <narendran.ranjit@gmail.com>
2018-05-03 15:52:10 -0700

    add single-watch for faster tests

--- build/watch.js
+++ build/watch.js
@@ -4,13 +4,17 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.config.set('watch', {
         source: {
-            files: ['src/**/*.js'],
-            tasks: ['test']
+            files: ['src/**/*.js', '!src/**/test-*.js'],
+            tasks: ['webpack:edge', 'karma:testList']
         },
-        // tests: {
-        //     files: ['tests/spec/**/*.js'],
-        //     tasks: ['mocha']
-        // },
+        tests: {
+            options: {
+                interrupt: true,
+                spawn: false
+            },
+            files: ['src/**/test-*.js'],
+            tasks: []
+        },
         components: {
             files: ['src/components/**/*', '!src/components/**/*.js'],
             tasks: ['copy:components']
@@ -18,6 +22,30 @@ module.exports = function (grunt) {
         templates: {
             files: ['src/components/**/templates/**/*.html'],
             tasks: ['templates']
+        }
+    });
+
+    var dependencies = grunt.config.get('karma.singleTest.files'); // keep the original files array
+    function handleSpecFileChanged(filepath) {
+        var testFilePath = './' + filepath.replace(/\\/g, '/');
+        grunt.log.writeln(['Running single karma test for: ' + testFilePath]);
+        var updatedFiles = dependencies.concat([{ src: filepath, 
+            watched: false,
+            included: true,
+            served: true, 
+        }]);
+        grunt.config.set('karma.singleTest.files', updatedFiles);
+
+        var pp = {};
+        pp[testFilePath] = ['webpack'];
+
+        grunt.config.set('karma.singleTest.options.preprocessors', pp);
+        grunt.task.run('karma:singleTest:start');
+    }
+
+    grunt.event.on('watch', function watchEventListener(action, filepath, target) {
+        if (target === 'tests' && action !== 'deleted') {
+            handleSpecFileChanged(filepath);
         }
     });
 };
