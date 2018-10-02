55e2fb2ce2bd1335e3636a74ae290e552307f0e5
Naren Ranjit <narendran.ranjit@gmail.com>
2018-10-02 16:17:14 -0700

    es6ify intro service

--- src/service/introspection-api-service/index.js
+++ src/service/introspection-api-service/index.js
@@ -1,5 +1,11 @@
+import TransportFactory from 'transport/http-transport-factory';
+import { getURLConfig, getDefaultOptions } from '../service-utils';
+
+var apiEndpoint = 'model/introspect';
+
 /**
- *
+ * @description
+ * 
  * ## Introspection API Service
  *
  * The Introspection API Service allows you to view a list of the variables and operations in a model. Typically used in conjunction with the [Run API Service](../run-api-service/).
@@ -12,60 +18,21 @@
  *       });
  *       intro.byModel('supply-chain.py').then(function(data){ ... });
  *       intro.byRunID('2b4d8f71-5c34-435a-8c16-9de674ab72e6').then(function(data){ ... });
- *
+ * 
+ * @param {AccountAPIServiceOptions} config 
  */
-
-'use strict';
-
-var ConfigService = require('service/configuration-service').default;
-var TransportFactory = require('transport/http-transport-factory').default;
-var SessionManager = require('store/session-manager');
-
-var apiEndpoint = 'model/introspect';
-
-module.exports = function (config) {
+export default function (config) {
     var defaults = {
-        /**
-         * For projects that require authentication, pass in the user access token (defaults to empty string). If the user is already logged in to Epicenter, the user access token is already set in a cookie and automatically loaded from there. (See [more background on access tokens](../../../project_access/)).
-         * @see [Authentication API Service](../auth-api-service/) for getting tokens.
-         * @type {string}
-         */
         token: undefined,
-
-        /**
-         * The account id. In the Epicenter UI, this is the **Team ID** (for team projects) or **User ID** (for personal projects). Defaults to empty string. If left undefined, taken from the URL.
-         * @type {string}
-         */
         account: undefined,
-
-        /**
-         * The project id. Defaults to empty string. If left undefined, taken from the URL.
-         * @type {string}
-         */
         project: undefined,
-
     };
 
-    var sessionManager = new SessionManager();
-    var serviceOptions = sessionManager.getMergedOptions(defaults, config);
-
-    var urlConfig = new ConfigService(serviceOptions).get('server');
-    if (serviceOptions.account) {
-        urlConfig.accountPath = serviceOptions.account;
-    }
-    if (serviceOptions.project) {
-        urlConfig.projectPath = serviceOptions.project;
-    }
-
-    var transportOptions = $.extend(true, {}, serviceOptions.transport, {
-        url: urlConfig.getAPIPath(apiEndpoint)
+    const serviceOptions = getDefaultOptions(defaults, config, {
+        apiEndpoint: apiEndpoint
     });
-    if (serviceOptions.token) {
-        transportOptions.headers = {
-            Authorization: 'Bearer ' + serviceOptions.token
-        };
-    }
-    var http = new TransportFactory(transportOptions);
+    const urlConfig = getURLConfig(serviceOptions);
+    var http = new TransportFactory(serviceOptions.transport);
 
     var publicAPI = {
         /**
