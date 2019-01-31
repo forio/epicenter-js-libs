3dddb5f22dcb2141b0f4d00d1de8de573944a003
Naren Ranjit <narendran.ranjit@gmail.com>
2019-01-31 12:45:45 -0800

    es6ify multiplayer strategy

--- src/managers/run-strategies/multiplayer-strategy.js
+++ src/managers/run-strategies/multiplayer-strategy.js
@@ -1,56 +1,48 @@
-var classFrom = require('../../util/inherit');
-
-var IdentityStrategy = require('./none-strategy');
-var WorldApiAdapter = require('../../service/world-api-adapter').default;
-
-var defaults = {};
+import WorldApiAdapter from 'service/world-api-adapter';
+import { rejectPromise } from 'util/index';
 
 /**
  * The `multiplayer` strategy is for use with [multiplayer worlds](../../../glossary/#world). It checks the current world for this end user, and always returns the current run for that world. This is equivalent to calling `getCurrentWorldForUser()` and then `getCurrentRunId()` from the [World API Adapater](../world-api-adapter/). If you use the [World Manager](../world-manager/), you are automatically using this strategy.
  * 
  * Using this strategy means that end users in projects with multiplayer worlds always see the most current world and run. This ensures that they are in sync with the other end users sharing their world and run. In turn, this allows for competitive or collaborative multiplayer projects.
  */
-var Strategy = classFrom(IdentityStrategy, {
-    constructor: function (options) {
+export default class MultiplayerStrategy {
+    constructor(options) {
+        const defaults = {};
         this.options = $.extend(true, {}, defaults, options);
         this.worldApi = new WorldApiAdapter(this.options.run);
-    },
+    }
 
-    reset: function (runService, session, options) {
-        var curUserId = session.userId;
-        var curGroupName = session.groupName;
-
-        var optionsToPassOn = $.extend(true, {}, options, {
+    reset(runService, session, options) {
+        const { userId, groupName } = session;
+        const optionsToPassOn = $.extend(true, {}, options, {
             success: $.noop,
         });
         return this.worldApi
-            .getCurrentWorldForUser(curUserId, curGroupName)
-            .then(function (world) {
+            .getCurrentWorldForUser(userId, groupName)
+            .then((world)=> {
                 return this.worldApi.newRunForWorld(world.id, optionsToPassOn).then(function (runid) {
-                    var toReturn = {
+                    const toReturn = {
                         id: runid
                     };
                     if (options && options.success) options.success(toReturn); 
                     return toReturn;
                 });
-            }.bind(this));
-    },
+            });
+    }
 
-    getRun: function (runService, session) {
-        var curUserId = session.userId;
-        var curGroupName = session.groupName;
-        var worldApi = this.worldApi;
-        var model = this.options.model;
-        var me = this;
-        var dtd = $.Deferred();
+    getRun(runService, session) {
+        const { userId, groupName } = session;
+        const worldApi = this.worldApi;
+        const model = this.options.model;
 
-        if (!curUserId) {
-            return dtd.reject({ statusCode: 401, type: 'UNAUTHORIZED', message: 'We need an authenticated user to join a multiplayer world. (ERR: no userId in session)' }, session).promise();
+        if (!userId) {
+            return rejectPromise('UNAUTHORIZED', 'We need an authenticated user to join a multiplayer world. (ERR: no userId in session)');
         }
 
-        var loadRunFromWorld = function (world) {
+        const loadRunFromWorld = function (world) {
             if (!world) {
-                return dtd.reject({ statusCode: 404, type: 'NO_WORLD_FOR_USER', message: 'The user is not in any world.' }, { options: me.options, session: session });
+                return rejectPromise('NO_WORLD_FOR_USER', `User ${userId} not part of a world`);
             }
             return worldApi.getCurrentRunId({ model: model, filter: world.id })
                 .then(function (id) {
@@ -59,24 +51,11 @@ var Strategy = classFrom(IdentityStrategy, {
                 .then(function (run) {
                     run.world = world;
                     return run;
-                })
-                .then(dtd.resolve)
-                .catch(dtd.reject);
+                });
         };
 
-        var serverError = function (error) {
-            // is this possible?
-            return dtd.reject(error, session, me.options);
-        };
-
-        this.worldApi
-            .getCurrentWorldForUser(curUserId, curGroupName)
-            .then(loadRunFromWorld)
-            .fail(serverError);
-
-        return dtd.promise();
-    },
-
-});
-
-module.exports = Strategy;
+        return this.worldApi
+            .getCurrentWorldForUser(userId, groupName)
+            .then(loadRunFromWorld);
+    }
+}
