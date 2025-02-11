e677a3e83048dcf280b77a594a45b033e5719d6c
Jacob Premo <jpremo@forio.com>
2025-02-11 16:35:32 -0500

    EVEREST3-1392 updated handling for localhost cookies

--- dist/epicenter.js
+++ dist/epicenter.js
@@ -5211,7 +5211,9 @@ module.exports = function (config) {
     var secureFlag = location.protocol === 'https:';
     var validHost = host.split('.').length > 1;
     var domain = validHost ? '.' + host : null;
-
+    if (host === 'localhost') {
+        domain = 'localhost';
+    }
     var defaults = {
         /**
          * Name of collection
@@ -5264,7 +5266,6 @@ module.exports = function (config) {
             var path = setOptions.root;
             var cookie = setOptions.cookie;
             var secureFlag = setOptions.secure;
-
             var contents = [encodeURIComponent(key) + '=' + encodeURIComponent(value)];
             if (domain) {
                 contents.push('domain=' + domain);
@@ -5277,7 +5278,7 @@ module.exports = function (config) {
             }
             if (samesite) {
                 contents.push('samesite=' + samesite);
-            } else if (domain === '.local.forio.com') {
+            } else if (domain === '.local.forio.com' || domain === 'localhost') {
                 contents.push('samesite=lax');
             } else {
                 contents.push('samesite=none');
@@ -8026,26 +8027,27 @@ function getURL(API_ENDPOINT, collection, doc, options) {
 /* 53 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (f) {
+var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function(f) {
 
   'use strict';
 
   /* istanbul ignore else */
-
-  if (typeof exports === 'object' && exports != null && typeof exports.nodeType !== 'number') {
-    module.exports = f();
+  if (typeof exports === 'object' && exports != null &&
+      typeof exports.nodeType !== 'number') {
+    module.exports = f ();
   } else if ("function" === 'function' && __webpack_require__(54) != null) {
     !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (f),
 				__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?
 				(__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__),
 				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
   } else {
-    var base64 = f();
+    var base64 = f ();
     var global = typeof self !== 'undefined' ? self : $.global;
     if (typeof global.btoa !== 'function') global.btoa = base64.btoa;
     if (typeof global.atob !== 'function') global.atob = base64.atob;
   }
-})(function () {
+
+} (function() {
 
   'use strict';
 
@@ -8054,69 +8056,66 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
   function InvalidCharacterError(message) {
     this.message = message;
   }
-  InvalidCharacterError.prototype = new Error();
+  InvalidCharacterError.prototype = new Error ();
   InvalidCharacterError.prototype.name = 'InvalidCharacterError';
 
   // encoder
   function btoa(input) {
-    var data = String(input),
-        o1,
-        o2,
-        o3,
-        bits,
-        i = 0,
-        acc = '';
+    var data = String (input), o1, o2, o3, bits, i = 0, acc = '';
 
     while (i < data.length) {
       // pack three octets into four hextets
-      o1 = data.charCodeAt(i++);
-      o2 = data.charCodeAt(i++);
-      o3 = data.charCodeAt(i++);
+      o1 = data.charCodeAt (i++);
+      o2 = data.charCodeAt (i++);
+      o3 = data.charCodeAt (i++);
 
-      if (o1 > 128 || o2 > 128 || o3 > 128) {
-        throw new InvalidCharacterError("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      if (o1 > 255 || o2 > 255 || o3 > 255) {
+        throw new InvalidCharacterError ("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
       }
 
-      bits = o1 << 16 | o2 << 8 | o3;
+      bits = (o1 << 16) | (o2 << 8) | o3;
       // use hextets to index into b64, and append result to encoded string
-      acc += chars.charAt(bits >> 18 & 0x3F) + chars.charAt(bits >> 12 & 0x3F) + chars.charAt(bits >> 6 & 0x3F) + chars.charAt(bits & 0x3F);
+      acc += chars.charAt ((bits >> 18) & 0x3F) +
+             chars.charAt ((bits >> 12) & 0x3F) +
+             chars.charAt ((bits >>  6) & 0x3F) +
+             chars.charAt ((bits)       & 0x3F);
     }
 
     switch (data.length % 3) {
-      case 0:
-        return acc;
-      case 1:
-        return acc.slice(0, -2) + '==';
-      case 2:
-        return acc.slice(0, -1) + '=';
+      case 0: return acc;
+      case 1: return acc.slice (0, -2) + '==';
+      case 2: return acc.slice (0, -1) + '=';
     }
   }
 
   // decoder
   // [https://gist.github.com/1020396] by [https://github.com/atk]
   function atob(input) {
-    var str = String(input).replace(/[=]+$/, ''); // #31: ExtendScript bad parse of /=
+    var str = (String (input)).replace (/[=]+$/, ''); // #31: ExtendScript bad parse of /=
     if (str.length % 4 === 1) {
-      throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+      throw new InvalidCharacterError ("'atob' failed: The string to be decoded is not correctly encoded.");
     }
     for (
-    // initialize result and counters
-    var bc = 0, bs, buffer, idx = 0, output = '';
-    // get next character
-    buffer = str.charAt(idx++); // eslint-disable-line no-cond-assign
-    // character found in table? initialize bit storage and add its ascii value;
-    ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-    // and if not first of each 4 characters,
-    // convert the first 8 bits to one ascii character
-    bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0) {
+      // initialize result and counters
+      var bc = 0, bs, buffer, idx = 0, output = '';
+      // get next character
+      buffer = str.charAt (idx++); // eslint-disable-line no-cond-assign
+      // character found in table? initialize bit storage and add its ascii value;
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        // and if not first of each 4 characters,
+        // convert the first 8 bits to one ascii character
+        bc++ % 4) ? output += String.fromCharCode (255 & bs >> (-2 * bc & 6)) : 0
+    ) {
       // try to find character in table (0-63, not found => -1)
-      buffer = chars.indexOf(buffer);
+      buffer = chars.indexOf (buffer);
     }
     return output;
   }
 
-  return { btoa: btoa, atob: atob };
-});
+  return {btoa: btoa, atob: atob};
+
+}));
+
 
 /***/ }),
 /* 54 */
@@ -9419,7 +9418,7 @@ var PasswordService = function () {
                 subject: 'Please reset your password',
                 projectFullName: 'My Awesome Project'
             });
-       * This will send the following email
+      * This will send the following email
      * 
      * Subject: Please reset your password
      * To: myuserName@gmail.com
@@ -9430,7 +9429,7 @@ var PasswordService = function () {
      * If you did not initiate this request, please ignore this email.
      * 
      * To reset your password, please click the following link: https://forio.com/epicenter/recover/<password recovery token>
-       * @param {string} userName user to reset password for 
+      * @param {string} userName user to reset password for 
      * @param {object} [resetParams] 
      * @param {string} [resetParams.redirectUrl] URL to redirect to after password is reset. Defaults to project root. If relative, it's treated as being relative to project
      * @param {string} [resetParams.subject] Subject for reset password email
